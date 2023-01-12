188c8a9a37d577eaf2b7b10a43a6562dad5bf67d
alo1719 <zhangxinyi1719@gmai.com>
2023-01-12 13:04:21 -0800

    add comments

--- 996.number-of-squareful-arrays.py
+++ 996.number-of-squareful-arrays.py
@@ -1,3 +1,54 @@
+#
+# @lc app=leetcode.cn id=996 lang=python3
+#
+# [996] Number of Squareful Arrays
+#
+# https://leetcode.cn/problems/number-of-squareful-arrays/description/
+#
+# algorithms
+# Hard (49.95%)
+# Likes:    95
+# Dislikes: 0
+# Total Accepted:    6.3K
+# Total Submissions: 12.7K
+# Testcase Example:  '[1,17,8]'
+#
+# An array is squareful if the sum of every pair of adjacent elements is a
+# perfect square.
+# 
+# Given an integer array nums, return the number of permutations of nums that
+# are squareful.
+# 
+# Two permutations perm1 and perm2 are different if there is some index i such
+# that perm1[i] != perm2[i].
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: nums = [1,17,8]
+# Output: 2
+# Explanation: [1,8,17] and [17,8,1] are the valid permutations.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: nums = [2,2,2]
+# Output: 1
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= nums.length <= 12
+# 0 <= nums[i] <= 10^9
+# 
+# 
+#
+
+# @lc code=start
 class Solution:
     def numSquarefulPerms(self, nums: List[int]) -> int:
         ans=0
@@ -13,3 +64,4 @@ class Solution:
                     dfs(nums[:i]+nums[i+1:],now+[nums[i]])
         dfs(nums,[])
         return ans
+# @lc code=end
